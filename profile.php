f1e4e5b91cf216cdadb6f07920eb132b4aed798f
tatum2003 <tatumyoyo@gmail.com>
2018-07-23 18:25:06 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 
 $access_token = 'WVx3/J6GszDkkn+mxzkWtEKyXE+8onO3I5v0GyNenyGvRqtMwYJ+9jMyP1hFpx1VAICfyFxSb5reQ5cUtcg3DvHWhDMFmVU3dcp8MVR3cTmUfKc2O/VK/cEdrqOs7ASS0KqFPvLdTN8yozW/grUSqwdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'f7b3f95a4e2916020f1043d30cc21d1e';
+$userId = 'U4bb746294c2aa26f319c694851fafca5';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
